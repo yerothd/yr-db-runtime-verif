c65012876e6121e3db87392b7131eb3d40e04f82
DR.-ING. DIPL.-INF. XAVIER NOUMBISSI NOUNDOU <yeroth.d@gmail.com>
2023-06-08 05:28:40 -0400

    UPDATED.

--- yr_db_runtime_verif_BUILD_DEBIAN_PACKAGE.sh
+++ yr_db_runtime_verif_BUILD_DEBIAN_PACKAGE.sh
@@ -5,48 +5,12 @@
 # CONSOLE AS THEY ARE EXECUTED.
 set -x
 
-# -------------------------------------------------------------- #
-YR_DB_RUNTIME_VERIF_deb_folder=yr-db-runtime-verif
-cp yr.db-runtime.verif.conf 		${YR_DB_RUNTIME_VERIF_deb_folder}/etc/dbus-1/system.d/yr.db-runtime.verif.conf
-cp yr-db-runtime-verif.service 	${YR_DB_RUNTIME_VERIF_deb_folder}/lib/systemd/system/yr-db-runtime-verif.service
-cp yr.db-runtime.verif.xml 			${YR_DB_RUNTIME_VERIF_deb_folder}/usr/share/dbus-1/interfaces/yr.db-runtime.verif.xml
-cp yr.db-runtime.verif.service 	${YR_DB_RUNTIME_VERIF_deb_folder}/usr/share/dbus-1/system-services/yr.db-runtime.verif.service
-# -------------------------------------------------------------- #
+rm -f yr-db-runtime-verif.deb
 
+cp YR-DB-RUNTIME-VERIF-GUI-ELEMENTS-SETUP/YEROTH_QVGE.pdf \
+	yr-db-runtime-verif/usr/share/doc/yr-db-runtime-verif
 
-#qdbusxml2cpp -a src/YRruntimeverification_adaptor yr.db-runtime.verif.xml 
-
-
-YR_ERRORS_LOG_FILE="yr_errors.log"
-
-clear && qmake -o Makefile yr-db-runtime-verif.pro
-
-make clean
-
-OUT_CMD_TIME=$(time make -j4 > /dev/null 2> "${YR_ERRORS_LOG_FILE}")
-
-val=$(cat "${YR_ERRORS_LOG_FILE}" | grep -w "*** No rule to make target\|error\|ERROR\|Error" | wc -l)
-
-if [ ${val} -eq 0 ]; then
-	#rm -f "${YR_ERRORS_LOG_FILE}" 
-	echo "[success build]"
+yeroth-create_yr-db-runtime-verif_deb-package.sh
 	
-	rm -f verif_adaptor.h
-	rm -f verif_adaptor.cpp
-	
-	rm -f yr-db-runtime-verif.deb
-
-	cp YR-DB-RUNTIME-VERIF-GUI-ELEMENTS-SETUP/YEROTH_QVGE.pdf \
-		yr-db-runtime-verif/usr/share/doc/yr-db-runtime-verif
-
-	yeroth-create_yr-db-runtime-verif_deb-package.sh
-	
-	cp yr.db-runtime.verif.xml ${YEROTH_ERP_3_0_HOME_FOLDER}/src/dbus/
-
-else
-	echo "[compilation avec des erreurs (${val})]"
-fi
-
-
-echo ${OUT_CMD_TIME}
+cp yr.db-runtime.verif.xml ${YEROTH_ERP_3_0_HOME_FOLDER}/src/dbus/
 
