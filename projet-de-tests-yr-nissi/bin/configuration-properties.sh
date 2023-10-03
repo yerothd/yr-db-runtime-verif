45c777ceb002c8e910e06b0a117fda0fc0290052
PROF. DR.-ING. DIPL.-INF. Xavier Noumbissi Noundou <yeroth.d@gmail.com>
2023-10-03 19:41:55 -0400

    UPDATED.

--- projet-de-tests-yr-nissi/bin/configuration-properties.sh
+++ projet-de-tests-yr-nissi/bin/configuration-properties.sh
@@ -1,4 +1,13 @@
 export YR_SD_MEALY_MACHINE_COMPILER_FULL_PATH=${HOME}/yr_sd_runtime_verif_lang/yr_sd_runtime_verif_lang_COMPILER__generation.sh
 export YR_SD_MEALY_MACHINE_SPEC_FOLDER_FULL_PATH=${HOME}/yr-db-runtime-verif/projet-de-tests-yr-nissi/sd-mealy-machine-specs
 export YR_DB_RUNTIME_VERIF_SRC_FULL_PATH=${HOME}/yr-db-runtime-verif/src
-export COMPILER_GENERATED_OUTPUT_FOLDER_FULL_PATH=${HOME}/yr-db-runtime-verif/runtime-monitor-cplusplus-classes-GENERATED
+
+YR_CURRENT_PROJECT_DIRECTORY=${PWD}
+YR_CURRENT_PROJECT_DIRECTORY_BASE=${YR_CURRENT_PROJECT_DIRECTORY%%/bin}
+
+#echo "* ${YR_CURRENT_PROJECT_DIRECTORY_BASE}"
+
+export COMPILER_GENERATED_OUTPUT_FOLDER_FULL_PATH=${YR_CURRENT_PROJECT_DIRECTORY_BASE}/runtime-monitor-cplusplus-classes-GENERATED
+
+#echo "**** ${COMPILER_GENERATED_OUTPUT_FOLDER_FULL_PATH}"
+
